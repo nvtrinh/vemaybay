a4a837acfac4ff7829971552831506b40d83bc0b
trinh <ngvantrinh2010@gmail.com>
2015-03-27 08:50:18 +0700

    header + footer

--- templates/airticket/index.php
+++ templates/airticket/index.php
@@ -1,6 +1,11 @@
 <?php
 defined('_JEXEC') or die;
 
+$doc             = JFactory::getDocument();
+
+
+// Add Stylesheets
+$doc->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/style.css');
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
@@ -9,6 +14,21 @@ defined('_JEXEC') or die;
     <jdoc:include type="head" />
 </head>
 <body class="site">
-    hello templates
+    <div class="banner">
+        <jdoc:include type="modules" name="vmb-banner1" style="none" />
+    </div>
+    <div class="clr"></div>
+    <div class="bounder">
+        <div class="main-menu">
+            <jdoc:include type="modules" name="vmb-main-menu" style="none" />
+        </div>
+        <div class="main-content">
+
+        </div>
+        <div class="clr"></div>
+        <div class="footer">
+            <jdoc:include type="modules" name="vmb-footer" style="none" />
+        </div>
+    </div>
 </body>
 </html>
